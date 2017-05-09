79552f766240a7345c47940dc1880906870a99b7
John Hensley <hensley@umich.edu>
2017-05-09 08:32:56 -0400

    Fix copyright notice

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'drmr'
-copyright = u'2015, The Parker Lab at the University of Michiganc'
+copyright = u'2015, The Parker Lab at the University of Michigan'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
